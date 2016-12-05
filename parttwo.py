61b37e4517986b6a88a0f4aa20ba633de7a06a0d
Florentin Hennecker <florentin.hennecker@gmail.com>
2016-12-05 17:01:25 +0100

    [add] boltzmann softmax

--- parttwo.py
+++ parttwo.py
@@ -3,6 +3,11 @@ import numpy as np
 def reward(grid, a, b):
     return np.random.randn(grid[b,a,0], grid[b,a,1])
 
+def boltzmann(q_values, tau):
+    q_values = q_values.flatten()
+    distribution = np.exp(q_values/tau)/np.sum(np.exp(q_values/tau))
+    return np.random.choice(range(q_values.size), p=distribution)
+
 def run(sigmas):
     sigma, sigma0, sigma1 = np.square(sigmas)
     grid = np.array([
