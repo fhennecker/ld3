e3ea1ed01439f1a6b0b1b015b55dd505b23696c2
Florentin Hennecker <florentin.hennecker@gmail.com>
2016-12-08 15:47:20 +0000

    [add] q_values function for part two

--- parttwo.py
+++ parttwo.py
@@ -8,6 +8,11 @@ def boltzmann(q_values, tau):
     distribution = np.exp(q_values/tau)/np.sum(np.exp(q_values/tau))
     return np.random.choice(range(q_values.size), p=distribution)
 
+def q_values(total_rewards, total_plays):
+    results = np.copy(total_rewards)
+    results[total_plays != 0] /= total_plays[total_plays != 0]
+    return results
+
 def run(sigmas):
     sigma, sigma0, sigma1 = np.square(sigmas)
     grid = np.array([
@@ -15,3 +20,6 @@ def run(sigmas):
         [[-30, sigma], [7, sigma1], [6, sigma]],
         [[0, sigma], [0, sigma], [5, sigma]]
     ])
+
+    total_rewards = np.zeros_like(grid)
+    total_plays = np.zeros_like(grid)
